9f296f83e94feb30a7f2740bbfb8766aab7929f1
Soff R <sofiiia.rojas16@gmail.com>
2023-12-09 16:12:53 -0600

    Auto stash before merge of "Dashboardconllaves" and "origin/Dashboardconllaves"
    firma y verificación

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -32,6 +32,15 @@ export default function Dashboard() {
   const [publick, setPublick] = useState('');
   const [publicKey, setPublicKey] = useState('');
   const [privateKey, setPrivateKey] = useState('');
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [signature, setSignature] = useState('');
+  const [verificationPublicKeyFile, setVerificationPublicKeyFile] = useState(null);
+
+
+  useEffect(() => {
+    // Generar llaves al cargar la página
+    generateKeys();
+  }, []);
 
   function generateKeys() {
     const keys = forge.pki.rsa.generateKeyPair({ bits: 2048 });
@@ -41,26 +50,10 @@ export default function Dashboard() {
     setPublicKey(publicKeyPem);
     setPrivateKey(privateKeyPem);
 
+    localStorage.setItem('privateKey', privateKeyPem); 
+    //localStorage.setItem('privateKey', privateKeyPem); 
     console.log('Public Key:', publicKeyPem);
     console.log('Private Key:', privateKeyPem);
-    const usuario = currentUser.uid;
-    ref.child(usuario).set({
-      publicKey: publicKeyPem ,
-      privateKey: privateKeyPem
-    });}
-
-
-    function showKey() {
-    var userRef = db.ref('users/' + currentUser.uid);
-    userRef.on('value', function(snapshot) {
-    // Recupera los datos del usuario
-    var usuario = snapshot.val();
-    console.log(usuario)
-    // Accede al valor específico que deseas, por ejemplo, el nombre
-    var publicks = usuario.publicKey;
-    console.log(publick)
-    setPublick(publicks);
-  });
   }
 
   async function handleLogout() {
@@ -73,6 +66,100 @@ export default function Dashboard() {
       setError('Hubo un fallo al salir')
     }
   }
+
+
+  function handleFileSelection(event) {
+    setSelectedFile(event.target.files[0]);
+  }
+
+  function handleVerificationKeySelection(event) {
+    setVerificationPublicKeyFile(event.target.files[0]);
+  }
+
+
+  function signFile() {
+    if (!selectedFile || !publicKey) {
+      console.error('Seleccione un archivo y genere una llave primero.');
+      return;
+    }
+  
+    const reader = new FileReader();
+    reader.onload = function () {
+      const fileContents = reader.result;
+  
+      const md = forge.md.sha256.create();
+      md.update(fileContents, 'utf8');
+      console.log("Hash antes de firmar", md.getBytes);
+  
+      const privateKey = forge.pki.privateKeyFromPem(localStorage.getItem('privateKey')); // Obtener la clave privada
+  
+      const signature = privateKey.sign(md); // Firmar el contenido del archivo
+  
+      // Crear un Blob con el contenido original y la firma al final, incluyendo delimitadores
+      const signedContent = fileContents + '\n\n-----BEGIN SIGNATURE-----\n' + signature + '\n-----END SIGNATURE-----\n';
+      const signedBlob = new Blob([signedContent], { type: 'text/plain' });
+  
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(signedBlob); // Establecer el Blob como el enlace de descarga
+      link.download = 'archivo_firmado.txt'; // Nombre del archivo descargado
+      link.click(); // Simular clic en el enlace para descargar
+  
+      console.log('FIRMA1:', signature);
+    };
+    reader.readAsText(selectedFile);
+  }
+  
+
+  //-**************VERIFICAR*********
+  function verifySignature() {
+    if (!selectedFile || !verificationPublicKeyFile) {
+      console.error('Seleccione un archivo y cargue el archivo de llave pública para verificar.');
+      return;
+    }
+  
+    const reader = new FileReader();
+    reader.onload = function () {
+      const fileContents = reader.result;
+  
+      const parts = fileContents.split('\n\n-----BEGIN SIGNATURE-----\n');
+      if (parts.length !== 2) {
+        console.error('El archivo no contiene una firma válida.');
+        return;
+      }
+  
+      const fileData = parts[0];
+      console.log(`contenido: ${fileData}}`);
+      const signature = parts[1].replace('-----END SIGNATURE-----\n', '');
+      console.log(`firma: ${signature}`);
+
+      const publicKeyReader = new FileReader();
+      publicKeyReader.onload = function () {
+        const publicKeyContents = publicKeyReader.result;
+        console.log(`llave pub: ${publicKeyContents}`);
+
+        const keys = forge.pki.publicKeyFromPem(publicKeyContents);
+        
+
+        const md = forge.md.sha256.create();
+        md.update(fileData, 'utf8');
+        const fileHash = md.digest().getBytes();
+        console.log('Hash del contenido:', fileHash);
+
+  
+        const isValid = keys.verify(fileHash, signature);
+        console.log('La firma es válida:', isValid);
+        if (isValid) {
+          alert('La firma es válida.');
+        } else {
+          alert('La firma no es válida.');
+        }
+      };
+      publicKeyReader.readAsText(verificationPublicKeyFile);
+    };
+    reader.readAsText(selectedFile);
+  }
+  
+
   
   return (
     <div className="hero">
@@ -127,23 +214,20 @@ export default function Dashboard() {
               </Card.Body>
             </Card>
           </div>
+        <input type="file" onChange={handleFileSelection} />
         </div>
-         <Button
-          type="button"
-          className="w-100 btn-lg btn-custom"
-          id="generate-keys"
-          onClick={generateKeys}
-        >
-          Genera llaves
-        </Button>
         <Button
           type="button"
-          className="w-100 btn-lg btn-custom"
-          id="generate-keys"
-          onClick={showKey}
+          className="mx-auto d-block w-30 btn-lg btn-outline-primary"
+          id="sign-txt"
+          onClick={signFile}
         >
-          Muestra llaves
+          Generar archivo firmado
         </Button>
+       <input type="file" onChange={handleFileSelection} />
+       <input type="file" onChange={handleVerificationKeySelection} />
+        <Button onClick={verifySignature}>Verificar Firma</Button>
+    
       </main>
       <p className='keys  '>{publick}</p>
       </div>
